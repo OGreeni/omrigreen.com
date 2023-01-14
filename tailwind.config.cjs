e3bcacff1b27406c6289ab2428256009beae2734
OGreeni <omri.green1@gmail.com>
2023-01-13 23:18:49 -0500

    add tailwind prettier plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('prettier-plugin-tailwindcss'), require('daisyui')],
   daisyui: {
     styled: true,
     themes: true,
